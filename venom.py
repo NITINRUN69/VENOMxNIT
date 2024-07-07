256a5c4fc7a00059ce730f998d01ef11f2d1455c
NITINRUN69 <lofihub31@gmail.com>
2024-07-08 05:22:22 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7207694784:AAHos4YdLb7VFM3gM0aXCmluJ6R2SPFLx6g'
+ADMIN_ID = '7102756010'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
